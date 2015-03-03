0f8cd1f90101879385af8251497321ed38564e8f
mohamedsamir92 <mhmdsamir92@gmail.com>
2015-03-03 17:22:23 +0200

    Some Changes

--- src/com/SWE/Test/TestServlet.java
+++ src/com/SWE/Test/TestServlet.java
@@ -8,6 +8,7 @@ public class TestServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 			throws IOException {
 		resp.setContentType("text/plain");
+		/* Some changes */
 		resp.getWriter().println("Hello, world");
 	}
 }
